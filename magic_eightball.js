28a24b6fe898dd3549461dd7c3bae843a51a5bbf
zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-18 11:54:02 -0400

    create magic_eightball.js
    create magic eight ball script

---
+++ magic_eightball.js
@@ -0,0 +1,31 @@
+const userName = 'Sam'
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'Will I be the oldest person alive?'
+console.log( `${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+if (randomNumber ===0) {
+  eightBall = 'It is certain'
+} else if (randomNumber === 1) {
+  eightBall = 'It is decidedly'
+} else if (randomNumber === 2) {
+  eightBall = 'Reply hazy try again'
+} else if (randomNumber === 3) {
+  eightBall = 'Cannot predict now'
+} else if (randomNumber === 4) {
+  eightBall = 'Do not count on it'
+} else if (randomNumber === 5) {
+  eightBall = 'My sources say no'
+} else if (randomNumber === 6) {
+  eightBall = 'Outlook not so good'
+} else if (randomNumber === 7) {
+  eightBall = 'Signs point to yes'
+} else {
+  console.log('Something is broken');
+}
+
+console.log(eightBall)
