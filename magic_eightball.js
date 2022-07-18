fd0c9e0ab63b11ac54c99049f5415ab51b943d3c
zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-18 12:06:07 -0400

    update magic_eightball
    changed if/else to switch method

--- magic_eightball.js
+++ magic_eightball.js
@@ -8,24 +8,34 @@ console.log( `${userName} asked: ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = '';
 
-if (randomNumber ===0) {
-  eightBall = 'It is certain'
-} else if (randomNumber === 1) {
-  eightBall = 'It is decidedly'
-} else if (randomNumber === 2) {
-  eightBall = 'Reply hazy try again'
-} else if (randomNumber === 3) {
-  eightBall = 'Cannot predict now'
-} else if (randomNumber === 4) {
-  eightBall = 'Do not count on it'
-} else if (randomNumber === 5) {
-  eightBall = 'My sources say no'
-} else if (randomNumber === 6) {
-  eightBall = 'Outlook not so good'
-} else if (randomNumber === 7) {
-  eightBall = 'Signs point to yes'
-} else {
-  console.log('Something is broken');
+switch (randomNumber){
+    case 0:
+        console.log('It is certain');
+        break;
+    case 1:
+        console.log('It is decidedly');
+        break;
+    case 2:
+        console.log('Reply hazy try again');
+        break;
+    case 3:
+        console.log('Cannot predict now');
+        break;
+    case 4:
+        console.log('Do not count on it');
+        break;
+    case 5:
+        console.log('My sources say no');
+        break;
+    case 6:
+        console.log('Outlook not so good');
+        break;
+    case 7:
+        console.log('Signs point to yes');
+        break;
+    default:
+        console.log('Something is broken');
+        break;
 }
 
 console.log(eightBall)
